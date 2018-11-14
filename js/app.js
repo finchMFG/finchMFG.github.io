91db69ed7661c43100d81c07d2f45fee5045eb17
finchMFG <chris-1752@hotmail.com>
2018-11-14 11:29:56 -0500

    Add Website Template ( WIP )

--- js/app.js
+++ js/app.js
@@ -3,7 +3,10 @@
 (function () {
     function init() {
         var router = new Router([
-            new Route('home', 'home.html', true),            
+            new Route('home', 'home.html', true),
+            new Route('websites', 'websites.html', true),
+            new Route('games', 'games.html', true),
+            new Route('blog', 'blog.html', true),            
         ]);
     }
     init();
